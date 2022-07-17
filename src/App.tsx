7751a5cbb83e1944f7e3e855f59f53c71eda01c8
eternaleight <eternaleight0@gmail.com>
2022-07-18 02:52:20 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import axios from "axios"
@@ -6,6 +6,14 @@ import axios from "axios"
 const App = () => {
   const [file, setFile] = useState<string>("")
   const [description, setDescription] = useState<string>("")
+  const [posts, setPosts] = useState<any>([])
+
+  useEffect(() => {
+    (async() => {
+      const result = await axios.get("/posts")
+      setPosts(result.data.posts)
+    })()
+  },[])
 
   const submit = async (e: any) => {
     e.preventDefault()
@@ -13,7 +21,7 @@ const App = () => {
     data.append("image", file)
     data.append("description", description)
     const result = await axios.post("/posts", data)
-    console.log(result)
+    setPosts([result.data, ...posts])
   }
 
   return (
@@ -23,14 +31,24 @@ const App = () => {
       </header>
       <form onSubmit={submit}>
         <input
-          onChange={(e: any) => setFile(e.target.files[0])}
+          onChange={(e: any) => {
+            setFile(e.target.files[0])
+          }}
           type="file"
           accept="image/*"
         ></input>
         <input onChange={(e: any) => setDescription(e.target.value)} type="text" placeholder="description" className='outline-[#1119] outline outline-[1px] rounded-[2px] m-2'></input>
         <button type="submit" className='text-white bg-black px-1 py-[2px] rounded-[2px]'>Submit</button>
       </form>
-      <main></main>
+      <main>
+        {posts.map((post: any) => (
+          <figure key={post.id}>
+            <img src={post.image_url}/>
+            <figcaption>{post.image_url}</figcaption>
+            <figcaption>{post.description}</figcaption>
+          </figure>
+        ))}
+      </main>
     </div>
   )
 }
